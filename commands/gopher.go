69977b14015b9d6051547f88ca185bcdcc2a7562
jolheiser <john.olheiser@gmail.com>
2021-09-20 10:44:24 -0500

    Make followup error new line
    Signed-off-by: jolheiser <john.olheiser@gmail.com>

--- commands/gopher.go
+++ commands/gopher.go
@@ -66,7 +66,10 @@ func handleGopher(ds *discordgo.Session, ic *discordgo.InteractionCreate) (*disc
 				err = playSound(ds, g.ID, vs.ChannelID)
 				if err != nil {
 					lit.Error("gopher: playing sound: %s", err)
-					if _, err := ds.FollowupMessageCreate(ds.State.User.ID, ic.Interaction, false, &discordgo.WebhookParams{Content: gopherErr.Error()}); err != nil {
+					followupErr := &discordgo.WebhookParams{
+						Content: gopherErr.Error(),
+					}
+					if _, err := ds.FollowupMessageCreate(ds.State.User.ID, ic.Interaction, false, followupErr); err != nil {
 						lit.Error("followup to interaction %s: %v", ic.ApplicationCommandData().Name, err)
 					}
 				}
